28461dcd8e75800e3dce399551d1ecf28c56efa7
diogorodriguees <diogorodrigueslife@gmail.com>
2025-03-02 22:33:37 -0300

    chore(ternary): removing unsed validation

--- src/architecture/handlers/error.handler.ts
+++ src/architecture/handlers/error.handler.ts
@@ -3,6 +3,6 @@ import { Logger } from '@nestjs/common';
 export class ErrorHandler {
   static execute(logger: Logger, msg: string, err: any) {
     logger.error(msg + ': ' + err);
-    return err.response && err.response.message ? err.response.message : null;
+    return err.response?.message ? err.response.message : null;
   }
 }
